81c235e2316862ea849b9f33cd0843cc94ce2958
absolutholz <absolutholz@gmail.com>
2021-07-11 13:16:54 +0200

    Update app to load clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import React from 'react';
-import styled from 'styled-components';
-
-// import { Clock } from './components/Clock';
-import { ClockSvg } from './components/ClockSvg';
-
-const StyledClock = styled.div`
-  /* margin: 4rem; */
-  /* width: 250px; */
-`;
+import { ClockQuestion } from './components/ClockQuestion/ClockQuestion';
 
 function App() {
   return (
     <>
-      <StyledClock>
-        {/* <Clock /> */}
-        <ClockSvg hours={10} minutes={10} />
-      </StyledClock>
+      <ClockQuestion />
     </>
   );
 }
